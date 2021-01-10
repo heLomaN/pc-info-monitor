e2f011be176ae756d8e378bff616d5d97502b195
heLomaN <caozhk@gmail.com>
2021-01-10 11:40:30 +0800

    add doc

--- report-local-info.py
+++ report-local-info.py
@@ -4,10 +4,9 @@ from iolibs.mailtool import get_latest_mail, send_mail_to_qq
 
 
 if __name__ == "__main__":
-    # send_mail_to_qq("report new ip", get_public_ip())
+    send_mail_to_qq("report new ip", get_public_ip())
 
     mail_text = get_latest_mail()
-    print(mail_text.content)
-
+    print("get mail:\n", mail_text.content)
 
     print("complete")
